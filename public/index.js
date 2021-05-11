57d3efbeeeac2c4782b2747f5b0c4acc8afa420f
Jason Davis <14043845+jasonericdavis@users.noreply.github.com>
2021-05-10 22:15:15 -0400

    added functionality to submit uploaded file

--- public/index.js
+++ public/index.js
@@ -1,14 +1,32 @@
-window.onload = () => {
+const socket = new WebSocket(`ws://${location.host}/socket`)
 
-    const socket = new WebSocket(`ws://${location.host}/socket`)
-
-    socket.onopen = function(e) {
-        console.log(`websocket connection has been established`)
-    }
-
-    socket.onmessage = function(e) {
-        console.log(e.data)
-        const greeting = document.getElementById('greeting')
-        greeting.innerHTML = e.data
-    }
+socket.onopen = function(e) {
+    console.log(`websocket connection has been established`)
 }
+
+socket.onmessage = function(e) {
+    console.log(e.data)
+    const greeting = document.getElementById('greeting')
+    greeting.innerHTML = e.data
+}
+
+const submitForm = event => {
+    event.preventDefault();
+    const formData = new FormData();
+    const mediaFile = document.getElementsByName("media")[0].files[0]
+    formData.append("media", mediaFile)
+
+    fetch("/upload_file", {
+        method: 'POST',
+        body: formData
+    })
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err))
+
+    console.log(`Form Submitted`);
+}
+
+const form = document.getElementById("form");
+form.addEventListener("submit", submitForm)
+
+
--- server.js
+++ server.js
@@ -1,14 +1,56 @@
 const express = require('express')
 const ws = require('ws')
+const multer = require('multer')
+const env = require('dotenv')
+const { RevAiApiClient } = require('revai-node-sdk');
 
+env.config();
+const access_token = process.env.access_token;
+const callback_url = process.env.callback_url;
+const upload = multer({ dest: 'uploads/' })
+
+// Setup the Rev.ai sdk
+const revai = new RevAiApiClient(access_token);
 
 const app = express()
 const port = 3000
-app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// This middleware has to be called before the routes
+app.use(express.json());
+
+/**
+ *  Notes: There is currently a 10MB limit on the size of the audio file because 
+ * of a limitiation in axios with the maxBodyLength and maxContentLength
+ */
+app.post('/upload_file', upload.single('media'), async (req, res, next) => {
+  console.log(req.file)
+  console.log(`callback_url: ${callback_url}`)
+  try {
+    const job = await revai.submitJobLocalFile(req.file.path, {
+      callback_url
+    })
+    res.json(job);
+  } catch(err) {
+    console.dir(`Error: ${err.message}`)
+    res.status(500).json({error: err.message})
+  }
 })
+
+app.post('/job_completed', (req, res) => {
+  console.log(`callback received: ${req.body}`)
+  // broadcast the message to all of the connected clients
+  // wss is a server that contains clients which are an array
+  // of all of the websocket connections
+  if(wss && wss.clients) {
+    wss.clients.forEach(client => {
+      client.send(JSON.stringify(req.body))
+    })
+  }
+  res.sendStatus(200)
+})
+
+
+app.use(express.static('public'))
 
 const server = app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
